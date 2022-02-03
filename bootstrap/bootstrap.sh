8ca43bd0761ee030e9e5f29aa1912342e1d15861
Taylor K <taylor.kirk@pm.me>
2022-02-03 12:43:14 -0800

    Update bootstrap.sh

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -11,6 +11,10 @@ esac
 # zredis
 sudo mkdir -p /usr/local/car/db/redia/
 
+sudo DEBIAN_FRONTEND=noninteractive apt install --yes cmake redis-server libhiredis-dev \
+                        tree gem libfreetype6-dev libfontconfig-dev \
+                        autoconf automake nodejs npm
+
 # rustup
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh \
     1
