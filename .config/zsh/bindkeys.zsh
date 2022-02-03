665dda30fa948c645858e4567fe53cf220a70a3c
Taylor K <taylor.kirk@pm.me>
2022-02-02 21:01:55 -0800

    Create bindkeys.zsh

---
+++ .config/zsh/bindkeys.zsh
@@ -0,0 +1,112 @@
+#!/usr/bin/env zsh
+#!/bin/zsh
+
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+zle -N edit-command-line
+
+zle -N zle-keymap-select 
+auto-fu-zle-keymap-select
+
+### History Substring Search ###
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+bindkey -M vicmd 'k' history-substring-search-up
+bindkey -M vicmd 'j' history-substring-search-down
+
+# fuzzy find: start to type
+bindkey "$terminfo[kcuu1]" up-line-or-beginning-search
+bindkey "$terminfo[kcud1]" down-line-or-beginning-search
+bindkey "$terminfo[cuu1]" up-line-or-beginning-search
+bindkey "$terminfo[cud1]" down-line-or-beginning-search
+
+# backward and forward word with option+left/right
+bindkey '^[^[[D' backward-word
+bindkey '^[b' backward-word
+bindkey '^[^[[C' forward-word
+bindkey '^[f' forward-word
+
+# to to the beggining/end of line with fn+left/right or home/end
+bindkey "${terminfo[khome]}" beginning-of-line
+bindkey '^[[H' beginning-of-line
+bindkey "${terminfo[kend]}" end-of-line
+bindkey '^[[F' end-of-line
+
+# delete char with backspaces and delete
+bindkey '^[[3~' delete-char
+bindkey '^?' backward-delete-char
+
+# delete word with ctrl+backspace
+bindkey '^[[3;5~' backward-delete-word
+# bindkey '^[[3~' backward-delete-word
+
+# edit command line in $EDITOR
+autoload -U edit-command-line
+zle -N edit-command-line
+bindkey '^e' edit-command-line
+
+# search history with fzf if installed, default otherwise
+if test -d /usr/local/opt/fzf/shell; then
+	# shellcheck disable=SC1091
+	. /usr/local/opt/fzf/shell/key-bindings.zsh
+else
+	bindkey '^R' history-incremental-search-backward
+fi
+
+
+# Show dots while waiting to complete. Useful for systems with slow net access,
+# like those places where they use giant, slow NFS solutions. (Hint.)
+expand-or-complete-with-dots() {
+echo -n "\e[31m......\e[0m"
+zle expand-or-complete
+zle redisplay
+}
+zle -N expand-or-complete-with-dots
+bindkey "^I" expand-or-complete-with-dots
+
+# This inserts a tab after completing a redirect. You want this.
+# (Source: http://www.zsh.org/mla/users/2006/msg00690.html)
+self-insert-redir() {
+integer l=$#LBUFFER
+zle self-insert
+(( $l >= $#LBUFFER )) && LBUFFER[-1]=" $LBUFFER[-1]"
+}
+zle -N self-insert-redir
+for op in \| \< \> \& ; do
+  bindkey "$op" self-insert-redir
+done
+
+# Automatically quote URLs when pasted
+autoload -U url-quote-magic
+zle -N self-insert url-quote-magic
+
+# ZSH KEYBINDINGS {{{1
+
+# First, primarily use emacs key bindings
+bindkey -e
+
+# One keystroke to cd ..
+bindkey -s '\eu' '\eq^Ucd ..; ls^M'
+
+# Smart less-adder
+bindkey -s "\el" "^E 2>&1|less^M"
+
+# This lets me use ^Z to toggle between open text editors.
+bindkey -s '^Z' '^Ufg^M'
+
+# Edit the current command line with Meta-e
+autoload -U edit-command-line
+zle -N edit-command-line
+bindkey '\ee' edit-command-line
+
+# Let ^W delete to slashes - zsh-users list, 4 Nov 2005
+# (I can't live without this)
+backward-delete-to-slash() {
+  local WORDCHARS=${WORDCHARS//\//}
+  zle .backward-delete-word
+}
+zle -N backward-delete-to-slash
+bindkey "^W" backward-delete-to-slash
+
+# AUTO_PUSHD is set so we can always use popd
+bindkey -s '\ep' '^Upopd >/dev/null; dirs -v^M'
