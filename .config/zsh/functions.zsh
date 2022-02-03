9ab46abbfe7088fbcb8f06eddb7b8cd010166f91
Taylor K <taylor.kirk@pm.me>
2022-02-02 22:38:48 -0800

    Create functions.zsh

---
+++ .config/zsh/functions.zsh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+SAVEIFS=$IFS
+IFS="$(pr  
+
+function apt-impgpg --keyserver subkeys.pgp.net --recv-keys $1 | gpg --armor --export $1 | sudo apt-key add -
+}
+
+# reloads all funf=(~/.config/zsh/functunfunction $f:t autoload -U $f:t
+}
+
+# creates new script, populating shebang line & executable.if i=printf '#!/usr/bin/env %s\n\n' $1 > $2 && chmod 755 $2 && vim + $2 && cecho "'which' could not find $1, is it in yo# in c is in path, this throw out the command ha#over  (man rehash
+}
