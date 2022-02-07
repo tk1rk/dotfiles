7476b37b65422337de91bcebd414ef753ef898fa
Taylor K <taylor.kirk@pm.me>
2022-02-06 19:13:50 -0800

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,30 @@
+require('maps')
+require('settings')
+require('status')
+require('utils')
+
+cmd 'packadd paq-nvim'               -- load the package manager
+local paq = require('paq-nvim').paq  -- a convenient alias
+-- This will load fzy_native and have it override the default file sorter
+require('telescope').load_extension('fzf_native')
+paq {'savq/paq-nvim', opt = true}    -- paq-nvim manages itself
+
+paq {
+    'nvim-treesitter/nvim-treesitter';
+    'neovim/nvim-lspconfig';
+    'junegunn/fzf', run = fn['fzf#install'];
+    'junegunn/fzf.vim';
+ 
+    'ojroques/nvim-lspfuzzy';
+    'nvim-lua/popup.nvim';
+    'nvim-telescope/telescope.nvim';
+    'nvim-telescope/telescope-fzf-writer.nvim';
+    'nvim-lua/plenary.nvim';
+    'rust-analyzer/rust-analyzer';
 
 
 
-lua require('plugins')`
+-- truecolor
+if has('nvim') || has('termguicolors')
+  set termguicolors
+endif
