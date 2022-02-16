81646b4df33628bb9f1f2ee8d9bd4c2d344bf414
Taylor K <taylor.kirk@pm.me>
2022-02-15 23:21:05 -0800

    Update plugins.lua

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -1,1 +1,51 @@
+local vim = vim
 
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+-- ensure that packer is installed
+local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+  execute 'packadd packer.nvim'
+end
+
+vim.cmd('packadd packer.nvim')
+
+local packer = require'packer'
+local util = require'packer.util'
+
+packer.init({
+  package_root = util.join_paths(vim.fn.stdpath('data'), 'site', 'pack')
+ })
+
+-- impatient
+require('impatient')
+require'impatient'.enable_profile()
+ 
+ --- startup and add configure plugins
+packer.startup(function()
+  local use = use
+   -- add you plugins here like:
+    -- Treesitter
+    use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'},
+    use 'sheerun/vim-polyglot'
+    use 'nvim-treesitter/nvim-treesitter-refactor'
+    use 'p00f/nvim-ts-rainbow'
+    
+    -- Impatient
+    use 'lewis6991/impatient.nvim'
+
+    -- lspconfig
+    use 'neovim/nvim-lspconfig'
+
+    -- fuzzy-finder
+    use 'cljoly/telescope-repo.nvim' 
+    use {'nvim-telescope/telescope.nvim',
+      requires = { {
+      'nvim-lua/plenary.nvim'} }
+}
+   
+   
+  end
+ )
