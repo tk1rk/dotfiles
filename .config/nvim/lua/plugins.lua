2b21fc9b46f30b3992f79672604ab11f9cb6016d
Taylor K <taylor.kirk@pm.me>
2022-02-15 21:29:48 -0800

    Update plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -18,17 +18,30 @@ local util = require'packer.util'
 packer.init({
   package_root = util.join_paths(vim.fn.stdpath('data'), 'site', 'pack')
  })
+
+require('impatient')
+require'impatient'.enable_profile()
  
  --- startup and add configure plugins
 packer.startup(function()
   local use = use
    -- add you plugins here like:
+    -- Treesitter
     use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'},
     use 'sheerun/vim-polyglot'
     use 'nvim-treesitter/nvim-treesitter-refactor'
     use 'p00f/nvim-ts-rainbow'
+    
+    -- Impatient
+    use 'lewis6991/impatient.nvim'
+
+    -- lspconfig
     use 'neovim/nvim-lspconfig'
-   
+
+    -- fuzzy-finder
+    use 'nvim-lua/plenary.nvim'
+    use 'nvim-telescope/telescope.nvim'
+    use 'cljoly/telescope-repo.nvim'
    
    
   end
