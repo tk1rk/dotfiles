54689ad69e2efa3a89591d2112e745e9ee617d2d
Taylor K <taylor.kirk@pm.me>
2022-02-21 20:47:38 -0800

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,55 @@
 
+--[[
+
+  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
+]]
+
+----------------------------------------------------------
+-- Import Lua modules
+----------------------------------------------------------
+require('settings')
+require('keymaps')
+require('colors')
+require('plugins')
+require('plugins/alpha-nvim')
+require('plugins/chadtree')
+require('plugins/indent-blankline')
+require('plugins/lualine')
+require('plugins/nvim-cmp')
+require('plugins/nvim-lspconfig')
+require('plugins/nvim-treesitter')
+require('plugins/plenary')
+require('plugins/telescope')
+require('plugins/')
+require('plugins/vista')
+
+
+-------------------------------------------------------------
+-- packer.nvim bootstrap
+-------------------------------------------------------------
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+-------------------------------------------------------------
+augroup packer_user_config
+  autocmd!
+  autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+augroup end
+
+
+
+
+
+
+
+
+
+
+local neogit = require('neogit') neogit.setup {}
