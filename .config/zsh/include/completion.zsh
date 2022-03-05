bdf676fddc5984eecb12e575c56db388a1fdd3ae
Taylor K <taylor.kirk@pm.me>
2022-03-04 22:38:32 -0800

    Update completion.zsh

--- .config/zsh/include/completion.zsh
+++ .config/zsh/include/completion.zsh
@@ -1,18 +1,5 @@
 #!/usr/bin/env zsh
 
-# Load more completions
-fpath=($DOTFILES/zsh/plugins/zsh-completions/src $fpath)
-
-# Should be called before compinit
-zmodload zsh/complist
-
-zle-line-init () {auto-fu-init;}
-zle -N zle-line-init
-
-# cache
-zstyle ':completion:*' use-cache on
-zstyle ':completion:*' cache-path "$ZSH_CACHE/.zcompcache"
-
 # fish like Auto suggestion
 autoload predict-on
 predict-toggle() {
@@ -22,7 +9,6 @@ zle -N predict-toggle
 bindkey '^Z'   predict-toggle
 zstyle ':predict' toggle true
 zstyle ':predict' verbose true
-
 ## Now the fix, setup these two hooks: ###
 pasteinit() {
   OLD_SELF_INSERT=${${(s.:.)widgets[self-insert]}[2,3]}
@@ -33,35 +19,34 @@ pastefinish() {
 }
 zstyle :bracketed-paste-magic paste-init pasteinit
 zstyle :bracketed-paste-magic paste-finish pastefinish
-
 # Load these ssh identities with the ssh module.
 zstyle ':zim:ssh' ids 'id_rsa' 'id_ecdsa' 'id_ed25519'
-
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
-
-# rehash if command not found (possibly recently installed)
-zstyle ':completion:*' rehash true
-
-# verbose
+# fuzzy matching of completions
+zstyle ':completion:*' completer _complete _match _approximate
+zstyle ':completion:*:match:*' original only
+zstyle -e ':completion:*:approximate:*' max-errors 'reply=($((($#PREFIX+$#SUFFIX)/3>7?7:($#PREFIX+$#SUFFIX)/3))numeric)'
+# Pretty Completions
+zstyle ':completion:*:matches' group 'yes'
+zstyle ':completion:*:options' description 'yes'
+zstyle ':completion:*:options' auto-description '%d'
+zstyle ':completion:*:corrections' format ' %F{green}-- %d (errors: %e) --%f'
+zstyle ':completion:*:descriptions' format ' %F{yellow}-- %d --%f'
+zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
+zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
+zstyle ':completion:*:default' list-prompt '%S%M matches%s'
+zstyle ':completion:*' format ' %F{yellow}-- %d --%f'
+zstyle ':completion:*' group-name ''
 zstyle ':completion:*' verbose yes
-
-# completer
-zstyle ':completion:*' complete-options true
-
-zstyle ':completion:*' use-perl true
-zstyle :compinstall filename '$ZDOTDIR/.zshrc'
-
-# Ignore useless files, like .pyc.
-zstyle ':completion:*:(all-|)files' ignored-patterns '(|*/).pyc'
-
-# Completing process IDs with menu selection.
-zstyle ':completion:*:*:kill:*' menu yes select
-zstyle ':completion:*:kill:*'   force-list always
-
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+zstyle ':completion:*:functions' ignored-patterns '(_*|pre(cmd|exec))'
+zstyle ':completion:*' use-cache true
+zstyle ':completion:*' rehash true
+# color for some apps
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 # fzf search display
 zstyle ':completion:*' fzf-search-display true
-
 # fzf-tab
 zstyle ':fzf-tab:*' query-string prefix first
 zstyle ':fzf-tab:complete:_zlua:*' query-string input
@@ -72,20 +57,16 @@ zstyle ':fzf-tab:complete:cd:*' fzf-preview 'exa -1 --color=always $realpath'  #
 zstyle ':fzf-tab:*' switch-group ',' '.'
 zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
 zstyle ':fzf-tab:*' popup-pad 0 0
-
 # git-checkout
 zstyle ':completion:*:git-checkout:*' sort false
-
 # exa
 zstyle ':completion:*:exa' file-sort modification
 zstyle ':completion:*:exa' sort false
-
-history-words
-zstyle ':completion:*:expand:*​' tag-order all-expansions            ​
-​zstyle ​'​:completion:*:history-words​'​ list ​false​                          ​
-​zstyle ​'​:completion:*:history-words​'​ menu yes                            
-​zstyle ​'​:completion:*:history-words​'​ remove-all-dups yes                 
-​zstyle ​'​:completion:*:history-words​'​ stop yes                            ​
-
+#history-words
+zstyle ':completion:*:expand:*' tag-order all-expansions            
+zstyle ':completion:*:history-words' list ​false​                          
+zstyle ':completion:*:history-words' menu yes                            
+zstyle ':completion:*:history-words' remove-all-dups yes                 
+zstyle ':completion:*:history-words' stop yes                            
 # bash completions
-source /etc/bash_completion
+source /usr/share/bash-completion
