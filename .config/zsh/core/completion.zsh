bd1da9fcd5b68de7177f6dbe531af89effcab2bb
Taylor K <taylor.kirk@pm.me>
2022-04-12 19:08:37 -0700

    Update completion.zsh

--- .config/zsh/core/completion.zsh
+++ .config/zsh/core/completion.zsh
@@ -13,74 +13,7 @@ bindkey '^P' predict-toggle
 zstyle ':predict' toggle true
 zstyle ':predict' verbose true
 
-#autoload -U predict-on
-#zle -N predict-on
-#zle -N predict-off
-#bindkey '^X^Z' predict-on
-#bindkey '^X^A' predict-off
-#zstyle ':predict' verbose 'yes'
-
-## Now the fix, setup these two hooks: ###
-pasteinit() {
-  OLD_SELF_INSERT=${${(s.:.)widgets[self-insert]}[2,3]}
-  zle -N self-insert url-quote-magic
-}
-
-pastefinish() {
-  zle -N self-insert $OLD_SELF_INSERT
-}
-zstyle :bracketed-paste-magic paste-init pasteinit
-zstyle :bracketed-paste-magic paste-finish pastefinish
-# Load these ssh identities with the ssh module.
-zstyle ':zim:ssh' ids 'id_rsa' 'id_ecdsa' 'id_ed25519'
-# pasting with tabs doesn't perform completion
-zstyle ':completion:*' insert-tab pending
-# fuzzy matching of completions
-zstyle ':completion:*' completer _complete _match _approximate
-zstyle ':completion:*:match:*' original only
-zstyle -e ':completion:*:approximate:*' max-errors 'reply=($((($#PREFIX+$#SUFFIX)/3>7?7:($#PREFIX+$#SUFFIX)/3))numeric)'
-# Pretty Completions
-zstyle ':completion:*:matches' group 'yes'
-zstyle ':completion:*:options' description 'yes'
-zstyle ':completion:*:options' auto-description '%d'
-zstyle ':completion:*:corrections' format ' %F{green}-- %d (errors: %e) --%f'
-zstyle ':completion:*:descriptions' format ' %F{yellow}-- %d --%f'
-zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
-zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
-zstyle ':completion:*:default' list-prompt '%S%M matches%s'
-zstyle ':completion:*' format ' %F{yellow}-- %d --%f'
-zstyle ':completion:*' group-name ''
-zstyle ':completion:*' verbose yes
-zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-zstyle ':completion:*:functions' ignored-patterns '(_*|pre(cmd|exec))'
-zstyle ':completion:*' use-cache true
-zstyle ':completion:*' rehash true
-# color for some apps
-zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
-# fzf search display
-zstyle ':completion:*' fzf-search-display true
-# fzf-tab
-zstyle ':fzf-tab:*' query-string prefix first
-zstyle ':fzf-tab:complete:_zlua:*' query-string input
-zstyle ':fzf-tab:*' continuous-trigger '/'
-zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm,cmd -w -w"
-zstyle ':fzf-tab:complete:kill:argument-rest' fzf-flags --preview=$extract'ps --pid=$in[(w)1] -o cmd --no-headers -w -w' --preview-window=down:3:wrap
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'exa -1 --color=always $realpath'  # disable for tmux-popup
-zstyle ':fzf-tab:*' switch-group ',' '.'
-zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
-zstyle ':fzf-tab:*' popup-pad 0 0
-# git-checkout
-zstyle ':completion:*:git-checkout:*' sort false
-# exa
-zstyle ':completion:*:exa' file-sort modification
-zstyle ':completion:*:exa' sort false
-#history-words
-zstyle ':completion:*:expand:*' tag-order all-expansions            
-zstyle ':completion:*:history-words' list ​false​                          
-zstyle ':completion:*:history-words' menu yes                            
-zstyle ':completion:*:history-words' remove-all-dups yes                 
-zstyle ':completion:*:history-words' stop yes                            
-# bash completions
+## bash completions
 source /usr/share/bash-completion
 
 compinit
