c735a98eaa00d32ec0a04e2ddbd14ecdfeb4895c
Taylor K <taylor.kirk@pm.me>
2022-05-20 19:23:21 -0700

    Update pipewire.sh

--- bootstrap/pipewire.sh
+++ bootstrap/pipewire.sh
@@ -13,36 +13,29 @@ ninja -C builddir
 
 sudo meson install
 
-sudo systemctl --user start pipewire.service pipewire.socket
+systemctl --user start pipewire.service pipewire.socket
 
-sudo systemctl --user start pipewire-media-session.service
+systemctl --user start pipewire-media-session.service
 
-sudo systemctl --user enable pipewire-media-session.service
+systemctl --user enable pipewire-media-session.service
 
 
 #alsa 
 #On Fedora:
-/usr/lib64/alsa-lib/libasound_module_pcm_pipewire.so
+#/usr/lib64/alsa-lib/libasound_module_pcm_pipewire.so
 #On Ubuntu:
-/usr/lib/x86_64-linux-gnu/alsa-lib/libasound_module_pcm_pipewire.so
+#/usr/lib/x86_64-linux-gnu/alsa-lib/libasound_module_pcm_pipewire.so
 #There is also a config file installed in:
 /usr/share/alsa/alsa.conf.d/50-pipewire.conf
 #The plugin will be picked up by alsa when the following files
 #are in /etc/alsa/conf.d/
-sudo mv /etc/alsa/conf.d/50-pipewire.conf -> /usr/share/alsa/alsa.conf.d/50-pipewire.conf
-sudo mv /etc/alsa/conf.d/99-pipewire-default.conf
+sudo mv /etc/alsa/conf.d/50-pipewire.conf /usr/share/alsa/alsa.conf.d/50-pipewire.conf
+sudo mv /etc/alsa/conf.d/99-pipewire-default.conf /usr/share/alsa/alsa.conf.d/
 
 
 #pulseaudio replacement (if necessary) p)
 #/usr/bin/pipewire-pulse
-sudo systemctl --user start pipewire-pulse.service pipewire-pulse.socket
-
-
-
-
-
-
-
+systemctl --user start pipewire-pulse.service pipewire-pulse.socket
 
 systemctl --user start pipewire.service \
                       pipewire.socket \
