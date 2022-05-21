ab18bea97cce877170275006be35b4ecf2f0cb79
Taylor K <taylor.kirk@pm.me>
2022-05-20 19:15:58 -0700

    Create arch-plymouth.sh

---
+++ bootstrap/arch-plymouth.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+sudo git clone https://github.com/dreamsmasher/Arch-Wireframe-Plymouth-Theme /usr/share/plymouth/themes/arch-wireframe
+
+sudo sudo plymouth-set-default-theme -R arch-wireframe
