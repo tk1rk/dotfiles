018bc51695728c7c9a8c0e722dba9a4cbd1426bd
Taylor K <taylor.kirk@pm.me>
2022-05-22 22:30:29 -0700

    Create omv.sh

---
+++ bootstrap/omv.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+sudo apt-get install --yes gnupg
+sudo wget -O "/etc/apt/trusted.gpg.d/openmediavault-archive-keyring.asc" https://packages.openmediavault.org/public/archive.key
+sudo apt-key add "/etc/apt/trusted.gpg.d/openmediavault-archive-keyring.asc"
+
+sudo cat <<EOF >> /etc/apt/sources.list.d/openmediavault.list
+deb https://packages.openmediavault.org/public shaitan main
+deb https://downloads.sourceforge.net/project/openmediavault/packages shaitan main
+## Uncomment the following line to add software from the proposed repository.
+deb https://packages.openmediavault.org/public shaitan-proposed main
+deb https://downloads.sourceforge.net/project/openmediavault/packages shaitan-proposed main
+## This software is not part of OpenMediaVault, but is offered by third-party
+## developers as a service to OpenMediaVault users.
+deb https://packages.openmediavault.org/public shaitan partner
+deb https://downloads.sourceforge.net/project/openmediavault/packages shaitan partner
+EOF
+
+export LANG=C.UTF-8
+export DEBIAN_FRONTEND=noninteractive
+export APT_LISTCHANGES_FRONTEND=none
+sudo apt-get update
+sudo apt-get --yes --auto-remove --show-upgraded \
+    --allow-downgrades --allow-change-held-packages \
+    --no-install-recommends \
+    --option DPkg::Options::="--force-confdef" \
+    --option DPkg::Options::="--force-confold" \
+    install openmediavault-keyring openmediavault
+
+omv-confdbadm populate
