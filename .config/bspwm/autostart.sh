4d9bf05fd213a5c81fd64c27b89f2d0ab2b7fa1c
Taylor K <taylor.kirk@pm.me>
2022-07-22 09:16:46 -0700

    Update autostart.sh

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
 
-# polkit client
-#pgrep lxpolkit || lxpolkit &
-#/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+# Run polkit daemon if doesn't started.
+[ -n "$(pidof xfce-polkit)" ] || /usr/lib/xfce-polkit/xfce-polkit &
 
 # ibus daemon
 pgrep ibus-daemon || ibus-daemon &
@@ -10,64 +9,58 @@ pgrep ibus-daemon || ibus-daemon &
 # nm-applet
 pgrep -u $USER -x nm-applet > /dev/null || (nm-applet &)
 
-# picom
-while pgrep -u $UID -x picom > /dev/null; do sleep 1; done
-picom -config "$HOME/.config/picom/picom.conf" &	
+# Terminate if picom is already running
+killall -q picom && while pgrep -u $UID -x picom >/dev/null; do sleep 1; done # Wait until the processes have been shut down
+picom --config "${HOME}/.config/picom/picom.conf" &
 
 # xfce4 power manager
 xfce4-power-manager &
 
 # dunst
 pkill dunst
-dunst -conf "$HOME/.config/dunst/dunstrc" &
+dunst -conf "${HOME}/.config/dunst/dunstrc" &
 	
 # window swallowing
 pgrep bspswallow || bspswallow &
+
+# Fix cursor.
+xsetroot -cursor_name left_ptr
 
 # xsettingsd
 xsettingsd &
 
 # Launch keybinding daemon
-sxhkd -conf "$HOME/.config/sxhkd/sxhkdrc" &
+sxhkd -conf "${HOME}/.config/sxhkd/sxhkdrc" &
 
 # touchpad
 xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Tapping Enabled" 1
 xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Natural Scrolling Enabled" 1
 
 # xrdb (themes)
-xrdb merge "$HOME/.Xresources" &
+xrdb -override "${HOME}/.Xresources" &
 
 # wallpaper
-feh --bg-fill "$HOME/.config/bspwm/wallpapers/arch.png" &
-
-# pulseaudio
-pulseaudio &
-
-# light-locker
-#pgrep light-locker || light-locker &
-
-# polybar
-pkill polybar
-polybar -c "$HOME/.config/polybar/launch.sh" --pwidgets & 
-
-# polybar autohide
-polybar-msg cmd hide | bspc config top_padding 0
-polybar-msg cmd show | bspc config top_padding <gaps>
+feh --bg-fill "${HOME}/.config/bspwm/assets/bloodfountain.jpg" &
 
 # Run EWW.
-eww -c "$HOME/.config/eww/bar" --restart open bar &
+"${HOME}/.local/bin/eww" -c "${HOME}/.config/eww/bar --restart open bar" &
 
-# eww_bar - close while fullscreen
-bspc subscribe node_state | while read -r _ _ _ _ state flag; do
-    if [ "$state" != "fullscreen" ]; then
-        continue
-    fi
-    if [ "$flag" == on ]; then
-        "$HOME/.local/bin/eww" -c "$HOME/.config/eww/bar" close-all
-  else
-        "$HOME/.local/bin/eww" -c "$HOME/.config/eww/bar" open bar
-    fi
-done &
+# Fix for eww bar not hiding on a fullscreen window.
+"${HOME}/.config/bspwm/bin/eww_fullscreen_fix.sh" &
+
+# Fix windows being below bar
+xdo lower -N eww-bar
+
+# alacritty
+CONFIG="$HOME/.config/alacritty/alacritty.yml"
+if [ "$1" == "-f" ]; then
+	alacritty --class 'alacritty-float,alacritty-float' --config-file "$CONFIG"
+elif [ "$1" == "-s" ]; then
+	alacritty --class 'Fullscreen,Fullscreen' --config-file "$CONFIG" -o window.startup_mode=fullscreen window.padding.x=30 window.padding.y=30 window.opacity=0.95 font.size=14
+else
+	alacritty --config-file "$CONFIG"
+fi
+
                              
 # Drop-down terminal (can swap with any app)
 bspc rule -a dropdown sticky=on state=floating hidden=on
