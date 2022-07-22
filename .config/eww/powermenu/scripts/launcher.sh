bad4c159a04ae25afb72642e594e464a288d66d2
Taylor K <taylor.kirk@pm.me>
2022-07-22 12:00:12 -0700

    Add files via upload

---
+++ .config/eww/powermenu/scripts/launcher.sh
@@ -0,0 +1,10 @@
+cd "$(head -1 ~/.config/./eww/directory.txt)"
+
+./eww close-all
+./eww open blur
+./eww open cross
+./eww open greet
+./eww open poweroff
+./eww open restart
+./eww open log_out
+./eww open lock
