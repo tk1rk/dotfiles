b43ae2ddf2fcc8e7defb4429341ad4064b6c6809
Taylor K <taylor.kirk@pm.me>
2022-07-22 11:58:03 -0700

    Create launcher.sh

---
+++ .config/eww/powermenu/launcher.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+cd "$(head -1 ~/.config/eww/directory.txt)"
+
+./eww open blur
+./eww open poweroff
+./eww open restart
+./eww open log_out
+./eww open lock
+./eww open greet
+./eww open cross
