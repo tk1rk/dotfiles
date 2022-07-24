aba44180f31998391f4b4e2036f896745d388384
Taylor K <taylor.kirk@pm.me>
2022-07-23 17:29:28 -0700

    Delete polybar.sh

--- bootstrap/polybar.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-mkdir -p $HOME/.config/polybar && git clone --depth=1 https://github.com/adi1090x/polybar-themes.git $HOME/.config/polybar/themes
-
-cd $HOME/.config/polybar/themes
-chmod +x setup.sh
-
-./setup.sh
-
