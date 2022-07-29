1e73d20b49d78953488fddcc8b6dfa18105d4b50
Taylor K <taylor.kirk@pm.me>
2022-07-29 09:22:06 -0700

    Update autostart.sh

--- .config/bspwm/bin/autostart.sh
+++ .config/bspwm/bin/autostart.sh
@@ -9,19 +9,24 @@ xfce4-power-manager &
 # ibus daemon
 pgrep ibus-daemon || ibus-daemon &
 
+# java
+wmname LG3D &
+
 # nm-applet
+pkill nm-applet
 pgrep -u $USER -x nm-applet > /dev/null || (nm-applet &)
 
 # Terminate if picom is already running
 killall -q picom && while pgrep -u $UID -x picom >/dev/null; do sleep 1; done # Wait until the processes have been shut down
-picom --config "${HOME}/.config/picom/picom.conf" &
+picom --config $HOME/.config/picom/picom.conf &
 
 # Launch keybinding daemon
-sxhkd -conf "${HOME}/.config/sxhkd/sxhkdrc" &
+pkill sxhkd
+sxhkd --config $HOME/.config/sxhkd/sxhkdrc &
 
 # dunst
 pkill dunst
-dunst -conf "${HOME}/.config/dunst/dunstrc" &
+dunst -config $HOME/.config/dunst/dunstrc &
 	
 # Enable Super Keys For Menu
 ksuperkey -e 'Super_L=Alt_L|F1' &
@@ -41,22 +46,22 @@ xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Tapping Enabled" 1
 xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Natural Scrolling Enabled" 1
 
 # xrdb (themes)
-xrdb -override "${HOME}/.Xresources" &
+xrdb $HOME/.Xresources &
 
 # wallpaper
-feh --bg-fill "${HOME}/.config/bspwm/assets/bloodfountain.jpg" &
+feh --bg-fill "${HOME}/.config/bspwm/wallpapera/bloodfountain.jpg" &
 
 # Run EWW.
-"${HOME}/.local/bin/eww" -c "${HOME}/.config/eww/bar --restart open bar" &
+eww -c $HOME/.config/eww/bar --restart open bar &
 
 # Fix for eww bar not hiding on a fullscreen window.
-"${HOME}/.config/bspwm/bin/eww_fullscreen_fix.sh" &
+$HOME/.config/bspwm/bin/eww_fullscreen_fix.sh &
 
 # Fix windows being below bar
 xdo lower -N eww-bar
 
 # alacritty
-CONFIG="$HOME/.config/alacritty/alacritty.yml"
+CONFIG=$HOME/.config/alacritty/alacritty.yml
 if [ "$1" == "-f" ]; then
 	alacritty --class 'alacritty-float,alacritty-float' --config-file "$CONFIG"
 elif [ "$1" == "-s" ]; then
