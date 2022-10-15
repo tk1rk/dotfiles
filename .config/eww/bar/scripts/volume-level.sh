645585f230d1ddea4cd2ab96105ee0cd7ab8cfb3
Taylor K <taylor.kirk@pm.me>
2022-10-14 20:18:03 -0700

    Delete .config/eww/bar/scripts directory

--- .config/eww/bar/scripts/volume-level.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-X=$(awk -F"[][]" '/dB/ { print $2 }' <(amixer sget Master))
-echo "${X%?}"
--- .config/eww/bar/scripts/volume-stats.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-[ "$(amixer sget 'Master' | grep '\[off\]')" ] && echo "ﱙ" || echo "ﱘ"
-
