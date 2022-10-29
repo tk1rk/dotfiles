2468d73629b6254678b4a2ee7ae963593d3d3d2b
Taylor K <taylor.kirk@pm.me>
2022-10-29 14:16:48 -0700

    Update plug.lua

--- .config/nvim/lua/plug.lua
+++ .config/nvim/lua/plug.lua
@@ -2,6 +2,15 @@
 
 return require('packer').startup(function(use)
   -- [[ Plugins Go Here ]]
+  use {                                              -- filesystem navigation
+    'kyazdani42/nvim-tree.lua',
+    requires = 'kyazdani42/nvim-web-devicons'        -- filesystem icons
+  }
+
+
+
+
+
 end,
 config = {
   package_root = vim.fn.stdpath('config') .. '/site/pack'
