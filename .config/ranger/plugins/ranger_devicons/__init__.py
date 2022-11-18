16fea9c50b6d4df70769483d2a63d97053dd62e2
Taylor K <taylor.kirk@pm.me>
2022-11-18 14:55:14 -0800

    Update __init__.py

--- .config/ranger/plugins/ranger_devicons/__init__.py
+++ .config/ranger/plugins/ranger_devicons/__init__.py
@@ -1,9 +1,6 @@
-import os
 import ranger.api
 from ranger.core.linemode import LinemodeBase
 from .devicons import *
-
-SEPARATOR = os.getenv('RANGER_DEVICONS_SEPARATOR', ' ')
 
 @ranger.api.register_linemode
 class DevIconsLinemode(LinemodeBase):
@@ -12,4 +9,11 @@ class DevIconsLinemode(LinemodeBase):
   uses_metadata = False
 
   def filetitle(self, file, metadata):
-    return devicon(file) + SEPARATOR + file.relative_path
+    return devicon(file) + ' ' + file.relative_path
+
+@ranger.api.register_linemode
+class DevIconsLinemodeFile(LinemodeBase):
+  name = "filename"
+
+  def filetitle(self, file, metadata):
+    return devicon(file) + ' ' + file.relative_path
