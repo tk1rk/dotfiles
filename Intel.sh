e993d018b79d24e5559a855d5a09afce15be36c5
Taylor K <taylor.kirk@pm.me>
2022-11-25 09:55:39 -0800

    Create Intel.sh

---
+++ Intel.sh
@@ -0,0 +1,6 @@
+yay -Syy --no-confirm libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel intel-ucode libva-utils
+
+
+yay - Syy --no-confirm xdotool wmctrl libinput-gestures
+
+gpasswd -a $USER input
