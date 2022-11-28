01f3b4cf99d60005213abb4cb1558b1939b4584c
Taylor K <taylor.kirk@pm.me>
2022-11-28 09:08:45 -0800

    Update autostart.sh

--- .config/hypr/bin/autostart.sh
+++ .config/hypr/bin/autostart.sh
@@ -7,34 +7,32 @@ exec-once=xsettingsd
 exec-once=ibus-daemon -drxR
 exec-once=/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
 #exec-once=/usr/lib/mate-polkit/polkit-mate-authentication-agent-1
-exec-once=xfce4-power-manager --daemon &
-exec-once=dunst -config ~/.config/dunst/dunstrc &
+exec-once=xfce4-power-manager --daemon
+exec-once=dunst -config ~/.config/dunst/dunstrc 
 exec-once=pipewire 
 exec-once=pipewire-pulse 
 exec-once=wireplumber 
 
 # Apps
-exec-once=hyprpaper
-exec-once=waybar
-exec-once=swaync
-exec-once=spotifyd
-exec-once=swaybg -i ~/.config/hypr/wallpapers/pacman.png
-exec-once=sxhkd -c ~/.config/sxhkd/sxhkdrc &
-exec-once=parcellite &
-exec-once=greenclip daemon &
+exec-once=swaybg -i ~/.config/wallpapers/pacman.png
+exec-once=sxhkd -c ~/.config/sxhkd/sxhkdrc 
+exec-once=parcellite 
+exec-once=greenclip daemon 
+exec-once=ksuperkey -e 'Super_L=Alt_L|F1' 
+exec-once=ksuperkey -e 'Super_R=Alt_L|F1' 
 
 # UI
-exec-once=xrdb -merge ~/.Xresources &
-exec-once=xsetroot -cursor_name left_ptr &
-exec-once=picom --config ~/.config/picom/picom.conf &
-exec-once=eww -c ~/.config/eww/Bar/ open --toggle panel &
-exec-once=eww -c ~/.config/eww/Indicators/ daemon &
-exec-once=eww -c ~/.config/eww/Control-Center/ daemon &
+exec-once=xrdb -merge ~/.Xresources 
+exec-once=xsetroot -cursor_name left_ptr 
+exec-once=picom --config ~/.config/picom/picom.conf 
+exec-once=eww -c ~/.config/eww/Bar/ open --toggle panel 
+exec-once=eww -c ~/.config/eww/Indicators/ daemon 
+exec-once=eww -c ~/.config/eww/Control-Center/ daemon 
 
 # Idle & Lockscreen
 exec swayidle -w \
-    timeout 300 'swaylock-fancy' \      
+    timeout 300 'swaylock-fancy-rapid 7 3' \      
     timeout 600 'swaymsg "output * dpms off"' \ 
-    resume 'swaymsg "output * dpms on"' 
-    before-sleep 'swaylock-fancy'
+    resume 'swaymsg "output * dpms on"' \
+    before-sleep 'swaylock-fancy-rapid 7 3'
 
