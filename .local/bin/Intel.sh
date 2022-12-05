d76ba22577788abc8a71ba40625b48a7f94d7547
Taylor K <taylor.kirk@pm.me>
2022-12-04 18:13:58 -0800

    Update and rename Intel.sh to .local/bin/Intel.sh

---
+++ .local/bin/Intel.sh
@@ -0,0 +1,6 @@
+yay -Su --noconfirm libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel intel-ucode libva-utils
+
+
+yay -Su --noconfirm xdotool wmctrl libinput-gestures
+
+gpasswd -a $USER input
