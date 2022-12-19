8deab3f39273ef33adf0b3666883fb0ca83e1034
Taylor K <taylor.kirk@pm.me>
2022-12-19 15:30:52 -0800

    Update behaviour.lua

--- .config/nvim/lua/core/behaviour.lua
+++ .config/nvim/lua/core/behaviour.lua
@@ -1,3 +1,5 @@
+
+
 vim.cmd [[autocmd VimEnter * syntax on]]
 local set = vim.opt
 set.termguicolors = true
@@ -10,8 +12,9 @@ set.nu = true
 set.wrap = false
 set.updatetime = 250
 set.mouse = 'a'
-set.clipboard = "unnamedplus"
+set.clipboard = "unnamedplus" 
 set.hlsearch = false
 set.showmode = false
 vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false, scope="cursor"})]]
+
