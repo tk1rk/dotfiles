f4f8770243fd738013ff206ff76a4543b408783a
Taylor K <taylor.kirk@pm.me>
2022-12-19 16:43:57 -0800

    Update plugins-setup.lua

--- .config/nvim/lua/plugins-setup.lua
+++ .config/nvim/lua/plugins-setup.lua
@@ -34,15 +34,24 @@ packer.init({
   },
 })
 
-# Plugins
-return require('packer').startup({ function(use)
-  use {'wbthomason/packer.nvim'}
+-- Install packer
+local install_path = vim.fn.stdpath 'data' .. '~/.local/share/nvim/site/pack/packer/start/packer.nvim'
+local is_bootstrap = false
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  is_bootstrap = true
+  vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd [[packadd packer.nvim]]
+end
 
-  -- startup dashboard
-  use {'glepnir/dashboard-nvim'}
+require('packer').startup(function(use)
+  -- Package manager
+  use 'wbthomason/packer.nvim'
+
+    -- startup dashboard
+  use 'glepnir/dashboard-nvim'
 
   -- Theme
-  use { 'Mofiqul/dracula.nvim' }
+  use 'Mofiqul/dracula.nvim' 
 
   -- Statusline
   use {'konapun/vacuumline.nvim', branch = 'next', requires = {
@@ -50,6 +59,62 @@ return require('packer').startup({ function(use)
     'kyazdani42/nvim-web-devicons', opt = true
   }, config = function() require('vacuumline').setup() end} -- Add this line to use defaults; otherwise, call `setup` with your config as described below wherever you configure your plugins
 
+  --file explorer
+  use {'nvim-tree/nvim-tree.lua', requires = {
+    'nvim-tree/nvim-web-devicons',
+    'konapun/vacuumline.nvim' }
+  }
+
+  -- LSP Configuration & Plugins
+  use {'neovim/nvim-lspconfig', requires = {
+    -- Automatically install LSPs to stdpath for neovim
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+    -- Useful status updates for LSP
+    'j-hui/fidget.nvim',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
+    'hrsh7th/nvim-cmp',
+      {"L3MON4D3/LuaSnip",
+        wants = {
+          "friendly-snippets",
+          "vim-snippets",
+        },
+      },
+    'rafamadriz/friendly-snippets',
+    'honza/vim-snippets',
+    'windwp/nvim-autopairs',
+    'windwp/nvim-ts-autotag',
+    'saadparwaiz1/cmp_luasnip'}
+    },
+  }
+
+  -- formatting and diagnostic
+  use {'jose-elias-alvarez/null-ls.nvim',
+    after = "nvim-lspconfig",
+    config = function()
+      require("custom.plugins.null-ls").setup()
+    end,
+  },
+
+  use { -- Autocompletion
+    'hrsh7th/nvim-cmp',
+    requires = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
+  }
+
+  use { -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+      pcall(require('nvim-treesitter.install').update { with_sync = true })
+    end,
+  }
+
+  use { -- Additional text objects via treesitter
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    after = 'nvim-treesitter',
+  }
 
   -- save readonly files
   use {'lambdalisue/suda.vim'}
@@ -69,40 +134,6 @@ return require('packer').startup({ function(use)
   -- formatting
   use {'gpanders/nvim-parinfer'}
 
-  -- formatting and diagnostic
-  use {'jose-elias-alvarez/null-ls.nvim',
-    after = "nvim-lspconfig",
-    config = function()
-      require("custom.plugins.null-ls").setup()
-    end,
-  },
-
-  --file explorer
-  use {'nvim-tree/nvim-tree.lua', requires = {
-    'nvim-tree/nvim-web-devicons',
-    'konapun/vacuumline.nvim' }
-  }
-
-  --code correction
-  use {'neovim/nvim-lspconfig', requires = {
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-path',
-    'hrsh7th/cmp-cmdline',
-    'hrsh7th/nvim-cmp',
-    {"L3MON4D3/LuaSnip",
-      wants = {
-        "friendly-snippets",
-        "vim-snippets",
-      },
-    },
-    'rafamadriz/friendly-snippets',
-    'honza/vim-snippets',
-    'windwp/nvim-autopairs',
-    'windwp/nvim-ts-autotag',
-    'saadparwaiz1/cmp_luasnip'}
-  }
-
   -- Syntax Highlighting
   use {'nvim-treesitter/nvim-treesitter',
     run = function()
@@ -111,7 +142,7 @@ return require('packer').startup({ function(use)
     end,
   }
 
-  use { 'b4skyx/serenade', 
+  use {'b4skyx/serenade', 
     wants = {'nvim-treesitter'},
     requires = {'nvim-treesitter/nvim-treesitter', 'cespare/vim-toml'}
   }
@@ -123,6 +154,9 @@ return require('packer').startup({ function(use)
   use {'nvim-telescope/telescope.nvim', 
     requires = {'nvim-lua/plenary.nvim'}
   }
+
+  -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
 
   -- icon picker
   use {'stevearc/dressing.nvim'}
@@ -145,3 +179,4 @@ end,
       end
     }
   } })
+\& 
