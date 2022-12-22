db2303dd9d22aaa48c532957af3deca15ec325a6
Taylor K <taylor.kirk@pm.me>
2022-12-22 08:00:32 -0800

    Update autostart.sh

--- .config/hypr/bin/autostart.sh
+++ .config/hypr/bin/autostart.sh
@@ -5,14 +5,13 @@ exec-once=systemctl --user import-environment DISPLAY WAYLAND_DISPLAY SWAYSOCK
 exec-once=hash dbus-update-activation-environment 2>/dev/null && dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK
 exec-once=ibus-daemon -drxR
 exec-once=/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
-exec-once=xfce4-power-manager --daemon
 exec-once=swaybg -i ~/.config/wallpapers/pacman.png
 exec-once=pipewire 
 exec-once=pipewire-pulse 
 exec-once=wireplumber 
 
 # Apps
-exec-once=swaybg -i ~/.config/wallpapers/pacman.png
+exec-once=xfce4-power-manager --daemon
 exec-once=parcellite 
 exec-once=greenclip daemon 
 
@@ -23,7 +22,9 @@ exec=gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
 exec=gsettings set org.gnome.desktop.interface font-name 'Lemonade 10'
 exec=gsettings set org.gnome.desktop.interface cursor-theme 'ArchCursorComplete'
 
-
 # Idle & Lockscreen
 exec=$HOME/.local/bin/lock.sh
 
+# eww bar
+exec-once=eww -c $HOME/.config/eww/bar open bar
+exec-once=polybar -q tray -c ~/.config/eww/bar/polybar_tray.ini &
