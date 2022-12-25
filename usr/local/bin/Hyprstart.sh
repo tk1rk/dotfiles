34c4994233d48ac2c05cd3b0f09a0fe41970bcc0
Taylor K <taylor.kirk@pm.me>
2022-12-24 21:31:06 -0800

    Update Hyprstart.sh

--- usr/local/bin/Hyprstart.sh
+++ usr/local/bin/Hyprstart.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
 # XDG
-export XDG_CURRENT_DESKTOP=Hyprland
-export XDG_SESSION_TYPE=wayland
-export XDG_SESSION_DESKTOP=Hyprland
+export XDG_CURRENT_DESKTOP="Hyprland"
+export XDG_SESSION_TYPE="wayland"
+export XDG_SESSION_DESKTOP="Hyprland"
 
 # QT
 export QT_AUTO_SCREEN_SCALE_FACTOR=1
 export QT_QPA_PLATFORM="wayland;xcb"
 export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
-export QT_QPA_PLATFORMTHEME=qt5ct
+export QT_QPA_PLATFORMTHEME="qt5ct"
 
 # Toolkit backend variables
 _GLX_GSYNC_ALLOWED
 export _JAVA_AWT_WM_NONEREPARENTING=1
-export SDL_VIDEODRIVER=wayland
-export CLUTTER_BACKEND=wayland
+export SDL_VIDEODRIVER="wayland"
+export CLUTTER_BACKEND="wayland"
 export GDK_BACKEND="wayland;x11"
 
 # Theme
-export GTK_THEME=Dracula
-export XCURSOR_THEME=ArchCursorComplete
+export GTK_THEME="Dracula"
+export XCURSOR_THEME="ArchCursorComplete"
 export XCURSOR_SIZE=32
 
 exec system-cat --identifier=wayland /usr/bin/Hyprland $@
