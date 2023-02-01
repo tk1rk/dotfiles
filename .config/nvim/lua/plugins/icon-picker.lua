07def085934d98333c7bec8256b72235bf2e4d47
Taylor K <taylor.kirk@pm.me>
2023-02-01 14:20:52 -0800

    Update and rename icon-picker.lua to init.lua

--- .config/nvim/lua/plugins/icon-picker.lua
+++
@@ -1,5 +0,0 @@
-local opts = { noremap = true, silent = true }
-
-vim.keymap.set("n", "<Leader><Leader>i", "<cmd>IconPickerNormal<cr>", opts)
-vim.keymap.set("n", "<Leader><Leader>y", "<cmd>IconPickerYank<cr>", opts) --> Yank the selected icon into register
-vim.keymap.set("i", "<C-i>", "<cmd>IconPickerInsert<cr>", opts)
