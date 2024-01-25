3e23d8016e0c88e0bd89ccdd1510c7bb2366ee83
Taylor K <taylor.kirk@pm.me>
2024-01-24 16:45:09 -0800

    Delete .local/bin/lock.sh

--- .local/bin/lock.sh
+++
@@ -1,5 +0,0 @@
-swayidle -w \
-    timeout 300 'swaylock-fancy-rapid 7 3' \      
-    timeout 600 'swaymsg "output * dpms off"' \ 
-    resume 'swaymsg "output * dpms on"' \
-    before-sleep 'swaylock-fancy-rapid 7 3'
