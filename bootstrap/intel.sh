7d6e4955146b8aed2e2380e8c91c4520dc3f1b08
Taylor K. <tkirk85@gmail.com>
2025-03-19 17:39:41 -0700

    Delete the file at 'bootstrap/intel.sh'

--- bootstrap/intel.sh
+++
@@ -1,6 +0,0 @@
-yay -Su --noconfirm libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel intel-ucode libva-utils
-
-
-yay -Su --noconfirm xdotool wmctrl libinput-gestures
-
-gpasswd -a $USER input
