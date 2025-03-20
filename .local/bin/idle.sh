1ae0c97f72ed8e00cb393d46f9d66333c2e971d6
Taylor K. <tkirk85@gmail.com>
2025-03-19 17:37:31 -0700

    Delete the file at '.local/bin/idle.sh'

--- .local/bin/idle.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-swayidle -w timeout 300 lockscreen.sh \
-    timeout 360 suspend.sh \
-    resume resume.sh \
-    before-sleep lockscreen.sh \
-    after-resume resume.sh
-
-exec = lid.sh 
-bindl = ,switch:Lid Switch,exec,lid.sh
